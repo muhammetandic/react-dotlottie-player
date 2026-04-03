773c381f7152f58b1e0eb22a1619a06f5b127de6
Muhammet Andiç <m.andic@oak.technology>
2026-04-03 17:55:13 +0300

    chore: remove previously tracked dist folders from git history

--- packages/react-dotlottie-player/dist/components/DotLottiePlayer.d.ts
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-import type { DotLottiePlayerProps } from '../types';
-export declare const DotLottiePlayer: React.ForwardRefExoticComponent<DotLottiePlayerProps & React.RefAttributes<HTMLDivElement>>;
--- packages/react-dotlottie-player/dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export { DotLottiePlayer } from './components/DotLottiePlayer';
-export type { DotLottiePlayerProps } from './types';
